f93150c8dab5934f04487dd3dd3bf6a989383941
Omar Aljohani <omerrr350@hotmail.com>
2025-08-28 16:48:49 +0300

    Forgot to add files

---
+++ src/lox/RPNPrinter.java
@@ -0,0 +1,41 @@
+package lox;
+
+public class RPNPrinter implements Expr.Visitor<String> {
+    void print(Expr expr) {
+        System.out.println(expr.accept(this));
+    }
+
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " +
+                expr.right.accept(this) + " " +
+                expr.operator.lexeme;
+    }
+
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " +
+                expr.operator.lexeme;
+    }
+
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
+
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+
+        new RPNPrinter().print(expression);
+    }
+
+}
